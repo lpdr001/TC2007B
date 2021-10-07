c49ca234b559ba249509417a228d9a66bec50dfd
RodoL0204 <79293479+RodoL0204@users.noreply.github.com>
2021-10-07 09:00:19 -0700

    n

--- ProjectoBloqueAppAmbulancias/RetoRecuperado/Reto01/Models/Sesiones.swift
+++ ProjectoBloqueAppAmbulancias/RetoRecuperado/Reto01/Models/Sesiones.swift
@@ -8,34 +8,48 @@
 import Foundation
 import Firebase
 
-class Sesion: Codable {
+struct Sesion: Codable {
     let id:String
     let cierre:Bool
-    let fecha,idUsuario,servicio: String
-    let numeroSesion:Int
-    init(cierre:Bool, fecha:String, idUsuario:String, servicio:String){
+    let evaluacion,idExpediente,idUsuario,servicio: String
+    let fecha:Timestamp
+    let cuota,numeroSesion:Int
+    
+    init(cierre:Bool, fecha:Timestamp, numeroSesion: Int, evaluacion:String, idUsuario:String, servicio:String, idExpediente:String, couta:Int){
+        
         self.cierre = cierre
-        self.fecha = fecha
+        self.evaluacion = evaluacion
+        self.idExpediente = idExpediente
         self.idUsuario = idUsuario
-        self.servicio = servicio
-        numeroSesion = 0
-        id = "1234"
-    }
-    init(id:String, cierre:Bool, fecha:String, numeroSesion:Int, idUsuario:String, servicio:String){
-        self.id = id
-        self.cierre = cierre
         self.fecha = fecha
-        self.idUsuario = idUsuario
         self.servicio = servicio
         self.numeroSesion = numeroSesion
+        self.cuota = couta
+        
+        id = "4376"
+    }
+    
+    init(id:String, cierre:Bool, fecha:Timestamp, numeroSesion: Int, evaluacion:String, idUsuario:String, servicio:String, idExpediente:String, couta:Int){
+        self.id = id
+        self.cierre = cierre
+        self.evaluacion = evaluacion
+        self.idExpediente = idExpediente
+        self.idUsuario = idUsuario
+        self.fecha = fecha
+        self.servicio = servicio
+        self.numeroSesion = numeroSesion
+        self.cuota = couta
     }
     init(aDoc: DocumentSnapshot){
         self.id = aDoc.documentID
         self.cierre = aDoc.get("cierre") as? Bool ?? false
-        self.fecha = aDoc.get("fecha") as? String ?? ""
+        self.fecha = aDoc.get("fecha") as? Timestamp ?? Timestamp.init(seconds: 0, nanoseconds: 0)
+        self.evaluacion = aDoc.get("evaluacion") as? String ?? ""
+        self.idExpediente = aDoc.get("idExpediente") as? String ?? ""
         self.idUsuario = aDoc.get("isUsuario") as? String ?? ""
         self.servicio = aDoc.get("servicio") as? String ?? ""
         self.numeroSesion = aDoc.get("numeroSesion")as? Int ?? 0
+        self.cuota = aDoc.get("cuota")as? Int ?? 0
     }
 }
 
