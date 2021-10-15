2ef0e4490c5666b2afae3237f623d1ebce39d798
lpdr001 <lpdr001@gmail.com>
2021-10-15 14:27:19 -0500

    yei

--- ProjectoBloqueAppAmbulancias/RetoE4/Reto01/ModuloInformación/New Group1/MotivoServPickerView.swift
+++ ProjectoBloqueAppAmbulancias/RetoE4/Reto01/ModuloInformación/New Group1/MotivoServPickerView.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class motivoServPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    var currentValue = ""
 
     let motivos = ["Abuso", "Adicción", "Ansiedad", "Baja Autoestima", "Codependencia", "Comunicación familiar", "Conflicto con hermano", "Conflicto con madre", "Conflicto con padre", "Dependencia", "Divorcio", "Duelo", "Duelo Grupal", "Enfermedad", "Enfermedad Crónico degenerativa", "Heridas de la infancia", "Identidad de género", "Infertilidad", "Infidelidad", "Intento de suicidio", "Miedo", "Pérdida de vienes materiales", "Pérdida de identidad", "Pérdida laboral", "Relación con los padres", "Ruptura de noviazgo", "Stress", "Trastorno Obsesivo", "Violación", "Violencia Intrafamiliar", "Violencia Psicológica", "Viudez", "Otro"]
 
@@ -25,7 +27,17 @@ class motivoServPickerView: UIPickerView, UIPickerViewDelegate, UIPickerViewData
     }
     
     func getSelected(_ pickerView: UIPickerView, selectedRow row: Int) -> String? {
+        currentValue = motivos[row]
+        
         return motivos[row]
+        
+        
+    }
+    
+    
+    func returnMotivo (){
+        
+        
     }
     /*
     // Only override draw() if you perform custom drawing.
